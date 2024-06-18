589efe5ad02593ac63c3fe7f9939db0fe8179dc4
angelGarza2004salas <125837002+angelGarza2004salas@users.noreply.github.com>
2024-06-18 13:24:07 -0600

    Add files via upload

---
+++ Actividad-4--U-3/Actividad 4/prueva 2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad_4
+{
+    public partial class Form1 : Form 
+    {
+        Operaciones2 oper;
+
+        public Form1()
+        {
+            InitializeComponent();
+            oper = new Operaciones2();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(txtA.Text);
+                double b = Convert.ToDouble(txtB.Text);
+                double resultado = oper.Sumar(a, b);
+                txtResultado.Text = resultado.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor ingresa valores numéricos válidos.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(txtA.Text);
+                double b = Convert.ToDouble(txtB.Text);
+                double resultado = oper.Restar(a, b);
+                txtResultado.Text = resultado.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor ingresa valores numéricos válidos.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
